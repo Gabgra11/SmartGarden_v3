1d0c26d3455049f09d13692f9784abd461cf72b4
Gabgra11 <gabrielgrais11@gmail.com>
2022-08-11 19:26:43 -0500

    Recreate UI with Bootstrap

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+
+@app.route("/stats")
+def stats():
+    return render_template("stats.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/donate")
+def donate():
+    return render_template("donate.html")
+"""
 @app.route("/<name>")
 def user(name):
     return f"hello {name}!"
@@ -13,6 +28,13 @@ def user(name):
 @app.route("/admin")
 def admin():
     return redirect(url_for("user", name="ADMIN"))
+
+@ap.route('/', methods=['GET', 'POST'])
+def parse_request():
+    data = request.form
+    return f"Data: {data}"
+"""
+
 
 if __name__ == "__main__":
     app.run()
---
+++ static/js/stats.js
@@ -0,0 +1,82 @@
+var temps_7day = [70.4, 71.0, 70.9, 69.9, 70.2, 71.0, 71.2];
+var humid_7day = [29.9, 30.1, 30.2, 29.8, 29.8, 30.1, 30.0];
+var moist_7day = [85.2, 66.4, 45.2, 88.8, 67.9, 51.3, 44.2];
+var water_7day = [true, false, false, true, false, false, false];
+var day_labels = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var bar_colors = ['#42aaff', '#42aaff', '#42aaff', '#42aaff', '#42aaff', '#42aaff', '#42aaff'];
+$(document).ready(function() {
+  fillTempChart();
+  fillHumidChart();
+  fillMoistChart();
+});
+
+
+
+function fillTempChart() {
+  var temps_chart = document.getElementById("tempBarChart").getContext('2d');
+
+  var myBarChart = new Chart(temps_chart, {
+      type: 'bar',
+      data: {
+        labels: day_labels,
+        datasets: [{
+          label: 'Temperature (F)',
+          data: temps_7day,
+          backgroundColor: bar_colors
+        }]
+      },
+      options: {
+        legend: {display: false},
+        title: {
+          display: true,
+          text: "7-Day Temperature History"
+        }
+      }
+    });
+}
+
+function fillMoistChart() {
+  var moist_chart = document.getElementById("moistBarChart").getContext('2d');
+
+  var myBarChart = new Chart(moist_chart, {
+      type: 'bar',
+      data: {
+        labels: day_labels,
+        datasets: [{
+          label: 'Moisture (%)',
+          data: moist_7day,
+          backgroundColor: bar_colors
+        }]
+      },
+      options: {
+        legend: {display: false},
+        title: {
+          display: true,
+          text: "7-Day Soil Moisture History"
+        }
+      }
+    });
+}
+
+function fillHumidChart() {
+  var humid_chart = document.getElementById("humidBarChart").getContext('2d');
+
+  var myBarChart = new Chart(humid_chart, {
+      type: 'bar',
+      data: {
+        labels: day_labels,
+        datasets: [{
+          label: 'Humidity (%)',
+          data: humid_7day,
+          backgroundColor: bar_colors
+        }]
+      },
+      options: {
+        legend: {display: false},
+        title: {
+          display: true,
+          text: "7-Day Ambient Humidity History"
+        }
+      }
+    });
+}
