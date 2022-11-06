03db5c2128c2e1b8719f5ce965a0d822e7ded851
Gabgra11 <gabrielgrais11@gmail.com>
2022-11-06 14:14:56 -0600

    Fix JWT handling for Google login

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 """
 TODO: Fill out readme.md setup
-TODO: Fill out About tab.
 TODO: Implement Google Authentication log-in
 TODO: Host website on github.io
 TODO: Implement live, 7-day data storage. CSV? SQLite?
 TODO: Implement live stream.
 TODO: Decrease footer height.
-TODO: Share header/footer nav across all files. (Is this possible?)
+TODO: Share header/footer nav across all files. jinja blocks extends
 TODO: Flesh out validate_login function
+TODO: Implement log out
+TODO: Pull stats data from db
+TODO: Config parser
+TODO: Hash load_user function, move to users.py
 """
 
 import config
 import user
-import sqlite3
 import os
-from flask import Flask, redirect, url_for, render_template, request, jsonify
+from flask import Flask, redirect, url_for, render_template, request
+import jwt
 from flask_login import LoginManager, login_user, current_user
 
 login_manager = LoginManager()
@@ -22,42 +25,36 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('client_secret')
 login_manager.init_app(app)
 
-# Initialize connection to SQLite3 db:
-con = sqlite3.connect("vote2grow.db")
-cur = con.cursor()
-
 @login_manager.user_loader
 def load_user(user_id=None):
-    # res = cur.execute("SELECT user_id FROM Users WHERE user_id == " + user_id)
-    curr_user_id = user_id
     return user.User(user_id)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
 @app.route("/stats")
 def stats():
     return render_template("stats.html")
 
-@app.route("/login")
-def login():
-    config_data = { 'client_id': config.client_id, 
-                    'login_uri': config.login_uri}
-    return render_template("login.html", data=config_data)
-
 @app.route("/vote")
-def vote(): # Default to anonymous user
-    if not current_user.is_authenticated:
-        return redirect(url_for('login'))
-    else:
-        return render_template("vote.html")
+def vote():
+    return render_template(
+        "vote.html",
+        client_id = config.client_id,
+        login_uri = config.login_uri,
+        user_id = current_user.get_id()
+    )
+
+@app.route("/login", methods=["POST"])
+def login():
+    credential = jwt.decode(request.form['credential'], options={"verify_signature": False}, algorithms="RS256")
+    login_user(user.User(credential["email"]))
+    return redirect(url_for("vote"))
 
 @app.route("/about")
 def about():
     return render_template("about.html")
-
 
 @app.route("/live")
 def live():
@@ -70,32 +67,6 @@ def tos():
 @app.route("/policy")
 def policy():
     return render_template("policy.html")
-
-@app.route('/loginhandler', methods=['POST'])
-def loginhandler():
-    # Parse the data passed in from Google Sign In:
-    request_data = request.get_data(as_text=True).split("&")
-    request_data = clean_request_data(request_data)
-
-    if (not validate_login(request_data)):
-        print("Error Validating Login")
-        # Do something, redirect to fail page?
-        return redirect(url_for('login'))
-    else:
-        curr_user = load_user(request_data[0])
-        login_user(curr_user, remember=True)
-        return redirect(url_for('vote'))
-
-def validate_login(request_data):
-    if (len(request_data) == 2):
-        return True
-
-# Trim each data element such that everything before and including "=" is removed
-def clean_request_data(request_data):
-    for i in range(len(request_data)):
-        request_data[i] = request_data[i][request_data[i].find("=")+1:]
-    return request_data
-
 
 if __name__ == "__main__":
     app.run()
--- static/js/stats.js
+++ static/js/stats.js
@@ -1,9 +1,11 @@
+// Dummy placeholder data:
 var temps_7day = [70.4, 71.0, 70.9, 69.9, 70.2, 71.0, 71.2];
 var humid_7day = [29.9, 30.1, 30.2, 29.8, 29.8, 30.1, 30.0];
 var moist_7day = [85.2, 66.4, 45.2, 88.8, 67.9, 51.3, 44.2];
 var water_7day = [true, false, false, true, false, false, false];
 var day_labels = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 var bar_colors = ['#42aaff', '#42aaff', '#42aaff', '#42aaff', '#42aaff', '#42aaff', '#42aaff'];
+
 $(document).ready(function() {
   fillTempChart();
   fillHumidChart();
