4e02cb3ad7b4e0b6d3b751550fd909435a38355a
Gabriel Grais <gabrielgrais11@gmail.com>
2023-06-16 14:26:10 -0500

    Add more db config options.

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 client_id = os.environ.get("client_id")
 client_secret = os.environ.get('client_secret')
 login_uri = os.environ.get("login_uri")
-db_name = "postgres"
-db_user = "postgres"
+db_host = os.environ.get("db_host")
+db_name = os.environ.get("db_name")
+db_user = os.environ.get("db_user")
 db_password = os.environ.get("db_password")
